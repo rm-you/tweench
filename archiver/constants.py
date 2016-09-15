e1d75612c4c786266a13fa2e6e6f3c38ac5139ba
Adam Harwell <flux.adam@gmail.com>
2016-09-15 04:12:27 -0700

    Fix HASHES regex (removed extra capture group)

--- archiver/constants.py
+++ archiver/constants.py
@@ -4,7 +4,7 @@ POST_MESSAGE = u'STORE_POST'
 # Regexes
 IMGUR_ALBUM = '^https?://(?:m\.|www\.)?imgur\.com/a/([a-zA-Z0-9]+)'
 IMGUR_GALLERY = '^https?://(?:m\.|www\.)?imgur\.com/gallery/([a-zA-Z0-9]+)'
-IMGUR_HASHES = '^https?://(?:m\.|www\.)?imgur\.com/(([a-zA-Z0-9]{5,7}[&,]?)+)'
+IMGUR_HASHES = '^https?://(?:m\.|www\.)?imgur\.com/((?:[a-zA-Z0-9]{5,7}[&,]?)+)'
 IMGUR_PAGE = 'https?://(?:www\.|m\.)?imgur\.com/(.*)'
 IMGUR_SINGLE = 'https?://(?:i\.|www\.|m\.)?imgur\.com/(.*)(?:\..*)'
 GFYCAT = 'https?://.*\.gfycat.com/(.*)'
